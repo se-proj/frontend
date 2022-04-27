775a9988873b9597167d39a3791b04f672888c4f
Shubh-RAZ <66317664+Shubh-RAZ@users.noreply.github.com>
2022-04-28 00:54:58 +0530

    added object in an array accessible from everywhere

--- coffee_break/create_object/index.mjs
+++ coffee_break/create_object/index.mjs
@@ -1,11 +1,14 @@
 import jsf from 'json-schema-faker'
 import fs from 'fs';
 
+let objectArray = [];
 const createObjectFile = (object, schema, n_intentional_right_cases) => {
 	const schemaAsObject = JSON.parse(schema);
 
 	var filePath = 'obj.txt'; 
 	fs.unlinkSync(filePath);
+
+	object = [];
 
 	let noOfData = n_intentional_right_cases;
 	for (let i = 0; i < noOfData; i++) {
@@ -13,10 +16,17 @@ const createObjectFile = (object, schema, n_intentional_right_cases) => {
 		let sobj = JSON.stringify(obj)
 		sobj = sobj + '\n';
 
+		objectArray.push(obj);
+
 		fs.appendFile(filePath, sobj, function (err) {
 			if (err) throw err;
 		});
 	}
 };
 
-export default createObjectFile;
+
+const getObjArray = () => {
+	return objectArray;
+}
+
+export {createObjectFile,getObjArray};
