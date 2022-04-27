baa9577eb635e49f7e0520e546d11121c3ee304c
Shubh-RAZ <66317664+Shubh-RAZ@users.noreply.github.com>
2022-04-27 12:19:37 +0530

    added cleaning of files before write

--- coffee_break/create_object/index.mjs
+++ coffee_break/create_object/index.mjs
@@ -4,13 +4,16 @@ import fs from 'fs';
 const createObjectFile = (object, schema, n_intentional_right_cases) => {
 	const schemaAsObject = JSON.parse(schema);
 
+	var filePath = 'obj.txt'; 
+	fs.unlinkSync(filePath);
+
 	let noOfData = n_intentional_right_cases;
 	for (let i = 0; i < noOfData; i++) {
 		const obj = jsf.generate(schemaAsObject);
 		let sobj = JSON.stringify(obj)
 		sobj = sobj + '\n';
 
-		fs.appendFile('obj.txt', sobj, function (err) {
+		fs.appendFile(filePath, sobj, function (err) {
 			if (err) throw err;
 		});
 	}
