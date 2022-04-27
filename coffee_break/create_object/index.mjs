2f94256746719e43ca7e38b328f574797b9bae5c
Shubh-RAZ <66317664+Shubh-RAZ@users.noreply.github.com>
2022-04-27 12:16:59 +0530

    added right test cases

--- coffee_break/create_object/index.mjs
+++ coffee_break/create_object/index.mjs
@@ -1,10 +1,10 @@
 import jsf from 'json-schema-faker'
 import fs from 'fs';
 
-const createObjectFile = (object, schema) => {
+const createObjectFile = (object, schema, n_intentional_right_cases) => {
 	const schemaAsObject = JSON.parse(schema);
 
-	let noOfData = 5;
+	let noOfData = n_intentional_right_cases;
 	for (let i = 0; i < noOfData; i++) {
 		const obj = jsf.generate(schemaAsObject);
 		let sobj = JSON.stringify(obj)
--- coffee_break/index.mjs
+++ coffee_break/index.mjs
@@ -8,7 +8,7 @@ const generate_test_files = test_settings => {
 		test_settings.server_settings,
 		test_settings.router_settings.router_file_name
 	);
-	createObjectFile(test_settings.test_settings, test_settings.schema);
+	createObjectFile(test_settings.test_settings, test_settings.schema, test_settings.n_intentional_right_cases);
 };
 
 export default generate_test_files;
